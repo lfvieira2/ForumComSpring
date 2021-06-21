10f36b5d0ac21a27eb96c893c3768eb13be49d0c
luiz <luiz>
2021-06-21 10:20:37 -0300

    atualizando versão do spring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO USUARIO(nome, email, senha) VALUES('mod', 'mod@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+
+INSERT INTO PEFIL(id, nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PEFIL(id, nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PEFIS(usuario_id, perfis_id) VALUES(1, 1);
+INSERT INTO USUARIO_PEFIS(usuario_id, perfis_id) VALUES(2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
